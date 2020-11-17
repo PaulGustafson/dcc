306aa06bfad392f4a4c01ae0ee1dc6e43c02c7cf
Paul Gustafson <paul.gustafson@gmail.com>
2020-11-16 23:46:15 -0500

    Fix some tests

--- Test.hs
+++ Test.hs
@@ -42,50 +42,67 @@ test_bal :: Balance
 test_bal = M.fromList $ zip test_addr $ repeat 100
 
 
-test_sig :: Tx -> Maybe (Encrypted a)
-test_sig tx = Just $ Encrypted (PublicKey "Fake public key" "Fake PKE algo") (show tx)
+test_enc :: (Signable a) => a -> Encrypted a
+test_enc c = Encrypted (PublicKey "Fake public key" "Fake PKE algo") (show c)
+
+test_sig :: (Signable a) => a -> Maybe (Encrypted a)
+test_sig = Just . test_enc
+
            
 test_entry = Entry "" (Address "alice" "fake_addr_algo") (-20) Nothing
 
-test_tx = Tx {
+test_tx0 = Tx {
   header = "Hello, world"
   , entries = [Entry "" alice (-20) Nothing, Entry "" louis 10 Nothing]
 }
 
-test_tx2 = test_tx { entries = [Entry "" alice (-20) (test_sig test_tx), Entry "" louis 10 Nothing] }
+test_tx = test_tx0 { entries = [Entry "" alice (-20) (test_sig test_tx0), Entry "" louis 10 Nothing] }
+
+
+test_validate_tx = validate test_sig_parse (test_enc test_tx0) (test_tx)
 
 test_block = Block {
-   txs = [test_tx2]
+   txs = [test_tx]
    , reward = Entry "" fred 1 Nothing
    , algo = test_algo1_src
    , nonce = "test_nonce"
 }
 
+-- test_block = test_blockp { reward = Entry "" fred 1 (test_sig test_blockp) }
+
+-- test_validate_blk =  validate test_sig_parse (test_enc test_block) (test_block)
+
 test_chain :: Chain
 test_chain = [test_block]
 
--- TODO
--- test_addBlock = addBlock test_block test_bal
+test_addBlock = addBlock test_sig_parse test_sig_parse test_block test_bal
 
--- -- initial conditions
--- test_ic = ("", 0, test_bal)
+-- initial conditions
+test_ic = ("", 0, test_bal)
 
--- test_eval = eval test_parser test_scale test_chain test_ic
--- test_hash = test_algo1 test_block
+test_eval = eval test_sig_parse test_sig_parse test_parser test_scale test_chain test_ic
+test_hash = test_algo1 test_block
 
--- test_tx2 = Tx {
---   header = "Transaction 2"
---   , entries = [Entry test_hash alice (-20) test_sig, Entry test_hash louis 10 test_sig]
--- }
+test_tx2p = Tx {
+  header = "Transaction 2"
+  , entries = [Entry test_hash alice (-20) Nothing, Entry test_hash louis 10 Nothing]
+}
+
+test_tx2 = test_tx2p { entries = [Entry "" alice (-20) (test_sig test_tx2p), Entry "" louis 10 Nothing] }
+
   
--- test_block2 = Block {
---    txs = [test_tx2]
---    , reward = Entry test_hash fred 2 test_sig
---    , algo = test_algo2_src
---    , nonce = "test_nonce2"
--- }
+test_block2p = Block {
+   txs = [test_tx2]
+   , reward = Entry test_hash fred 2 Nothing
+   , algo = test_algo2_src
+   , nonce = "test_nonce2"
+}
 
--- test_chain2 :: Chain
--- test_chain2 = [test_block2, test_block]
+test_block2 = test_block2p { reward = Entry test_hash fred 2 (test_sig test_block2p)}
 
--- test_eval2 = eval test_parser test_scale test_chain2 test_ic
+test_chain2 :: Chain
+test_chain2 = [test_block2, test_block]
+
+-- FIXME
+test_eval2 = eval test_sig_parse test_sig_parse test_parser test_scale test_chain2 test_ic
+
